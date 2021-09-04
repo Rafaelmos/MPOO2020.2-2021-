f7d47bfc5fcad2564226577dc62c65db3e7a67a7
Rafaelmos <rafael.rjms0@gmail.com>
2021-09-03 21:44:14 -0300

    Questao 11_1 (String[] args)
    Entendendo o funcionamento os parâmetro Args do metódo main.

---
+++ atividade1_codigos/src/questao11_1/Main.java
@@ -0,0 +1,13 @@
+package questao11_1;
+
+public class Main {
+	// Questão 11
+
+	public static void main(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+
+	}
+}
+//Executar no CMD Java Main.java Hello Rafael, essa é a reposta da questão 11 de Mpoo.
